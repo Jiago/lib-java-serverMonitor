bc8528f1b1e639154d3dfcb665751bf01d5b2205
Alex Jiang <zengzhu.jiang@landesk.com>
2015-08-03 09:51:47 +0800

    first commit

---
+++ src/cn/freecoder/jgo/monitor/ServerMonitor.java
@@ -0,0 +1,29 @@
+package cn.freecoder.jgo.monitor;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class ServerMonitor {
+
+	public static void main(String[] args) {
+		OperatingSystemMXBean operatingSystemMXBean = ManagementFactory
+				.getOperatingSystemMXBean();
+		for (Method method : operatingSystemMXBean.getClass()
+				.getDeclaredMethods()) {
+			method.setAccessible(true);
+			if (method.getName().startsWith("get")
+					&& Modifier.isPublic(method.getModifiers())) {
+				Object value;
+				try {
+					value = method.invoke(operatingSystemMXBean);
+				} catch (Exception e) {
+					value = e;
+				}
+				System.out.println(method.getName() + " = " + value);
+			}
+		}
+	}
+
+}
